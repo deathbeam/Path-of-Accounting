6e5e128c0f161f0c0f3168a255161142be68e2a1
rhzk <rhzkx@outlook.com>
2020-01-30 05:45:38 +0100

    changes..

---
+++ gui/guiComponent.py
@@ -0,0 +1,120 @@
+import os
+from tkinter import *
+import screeninfo
+# We do not need this on Linux.
+if os.name == "nt":
+    import pythoncom
+    import win32com.client
+    import win32gui
+
+
+def windowEnumerationHandler(hwnd, top_windows):
+    """
+    Handler for Windows OS enumeration of all open windows.
+    Used to return to the Path of Exile window after displaying the overlay.
+    """
+    top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+
+def windowToFront(root):
+    # This is necessary for displaying the GUI window above active window(s) on the Windows OS
+    if os.name == "nt":
+        # In order to prevent SetForegroundWindow from erroring, we must satisfy the requirements
+        # listed here:
+        # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setforegroundwindow
+        # We satisfy this by internally sending the alt character so that Windows believes we are
+        # an active window.
+        # We need this pythoncom call for win32com use in a thread.
+        pythoncom.CoInitialize()
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys("%")
+        win32gui.SetForegroundWindow(root.winfo_id())
+
+
+def windowRefocus(name):
+    """
+    Restore focus to a window, if on Windows.
+    TODO: If originating window was NOT "name", return to previous window.
+    """
+
+    if os.name == "nt":
+        results = []
+        top_windows = []
+        win32gui.EnumWindows(windowEnumerationHandler, top_windows)
+        for i in top_windows:
+            if name == i[1].lower():
+                win32gui.ShowWindow(i[0], 5)
+                win32gui.SetForegroundWindow(i[0])
+                break
+
+class GuiComponent:
+    def __init__(self):
+        self.frame = None
+        self.closed = True
+    def prepare_window(self):
+        tk = Tk().withdraw()
+        frame = Toplevel()
+        frame.overrideredirect(True)
+        frame.option_add("*Font", "courier 12")
+        frame.withdraw()
+        self.frame = frame
+
+    def is_closed(self):
+        if self.closed:
+            return True
+        return False
+
+    def close(self):
+        if self.closed:
+            return
+        self.closed = True
+        self.frame.destroy()
+        self.frame.update()
+        self.frame.quit()
+        self.frame = None
+    def add_components(self):
+        pass
+    def show(self,x_cord, y_cord):
+        self.closed = False
+        self.prepare_window()
+        self.add_components()
+        self.frame.update()
+        windowToFront(self.frame)
+        self.frame.deiconify()
+        self.frame.geometry(f"+{x_cord}+{y_cord}")
+        self.frame.update()
+
+    def show_at_cursor(self):
+        self.closed = False
+        self.prepare_window()
+        self.add_components()
+        self.frame.update()
+        if self.frame == None:
+            print("ERROR")
+        windowToFront(self.frame)
+
+        m_x = self.frame.winfo_pointerx()
+        m_y = self.frame.winfo_pointery()
+        def get_monitor_from_coord(x, y):
+            monitors = screeninfo.get_monitors()
+            for m in reversed(monitors):
+                if m.x <= x <= m.width + m.x and m.y <= y <= m.height + m.y:
+                    return m
+            return monitors[0]
+
+        # Get the screen which contains top
+        current_screen = get_monitor_from_coord(self.frame.winfo_x(), self.frame.winfo_y())
+        # Get the monitor's size
+        root_w = self.frame.winfo_width()
+        root_h = self.frame.winfo_height()
+
+        if m_x + root_w >= current_screen.width:
+            m_x = current_screen.width - root_w - 5
+
+        if m_y + root_h >= current_screen.height:
+            m_y = current_screen.height - root_h - 5
+
+        self.frame.deiconify()
+        self.frame.geometry(f"+{m_x}+{m_y}")
+        self.frame.update()
+
---
+++ gui/priceInfo.py
@@ -0,0 +1,103 @@
+from gui.guiComponent import *
+import time
+class PriceInfo(GuiComponent):
+    def __init__(self):
+        super().__init__()
+        self.price = []
+        self.price_vals = []
+        self.avg_times = []
+        self.opened = time.time()
+        self.elapsed = 0
+    def add_price_info(self, price, price_vals, avg_times):
+        if not self.is_closed():
+            self.close()
+        self.opened = time.time()
+        self.price = price
+        self.price_vals = price_vals
+        self.avg_times = avg_times
+    def should_close(self):
+        if self.is_closed():
+            return
+        self.elapsed = time.time() - self.opened
+        if self.elapsed >= 3:
+            elapsed = 0
+            self.close()
+    def add_components(self):
+        # Setting up Master Frame, only currently used for background color due to grid format.
+        masterFrame = Frame(self.frame, bg="#1f1f1f")
+        masterFrame.place(relwidth=1, relheight=1)
+
+        spacerLabel = Label(self.frame, text="   ", bg="#0d0d0d")
+        spacerLabel.grid(column=0, row=0, columnspan=3, sticky="w" + "e")
+
+        # Setting up header row of labels.
+        bglabel = Label(self.frame, bg="#0d0d0d").grid(column=0, row=1, columnspan=3, sticky="w" + "e")
+        headerLabel = Label(self.frame, text="Listed Price:", bg="#0d0d0d", fg="#e6b800").grid(column=0, row=1, padx=5)
+        headerLabel2 = Label(self.frame, text="Avg. Time Listed:", bg="#0d0d0d", fg="#e6b800").grid(
+            column=2, row=1, padx=5
+        )
+        headerLabel3 = Label(self.frame, text="   ", bg="#0d0d0d", fg="#e6b800").grid(column=1, row=1, sticky="w" + "e")
+
+        rows_used = len(self.price_vals)
+
+        for row in range(rows_used):
+            days = self.avg_times[row][0]
+            if days > 0:
+                days = str(days) + " days, "
+            else:
+                days = None
+
+            hours = None
+            if self.avg_times[row][1] > 3600:
+                hours = str(round(self.avg_times[row][1] / 3600, 2)) + " hours"
+            else:
+                hours = "< 1 hour"
+
+            if days is not None:
+                avg_time_text = days + hours
+            else:
+                avg_time_text = hours
+
+            # Alternates row color.
+            if row % 2 == 0:
+                # Needed here because other color is consistent with canvas color.
+                bgAltLabel = Label(self.frame, bg="#1a1a1a").grid(column=0, row=2 + row, columnspan=3, sticky="w" + "e")
+                priceLabel = Label(self.frame, text=self.price_vals[row], bg="#1a1a1a", fg="#e6b800").grid(
+                    column=0, row=2 + row, sticky="w", padx=5
+                )
+                avgTimeLabel = Label(self.frame, text=avg_time_text, bg="#1a1a1a", fg="#e6b800").grid(
+                    column=2, row=2 + row, sticky="w", padx=5
+                )
+            else:
+                priceLabel = Label(self.frame, text=self.price_vals[row], bg="#1f1f1f", fg="#e6b800").grid(
+                    column=0, row=2 + row, sticky="w", padx=5
+                )
+                avgTimeLabel = Label(self.frame, text=avg_time_text, bg="#1f1f1f", fg="#e6b800").grid(
+                    column=2, row=2 + row, sticky="w", padx=5
+                )
+
+            footerbgLabel = Label(self.frame, bg="#0d0d0d").grid(column=0, row=rows_used + 3, columnspan=3, sticky="w" + "e")
+
+            minPriceLabel = Label(self.frame, text="Low: " + str(self.price[0]), bg="#0d0d0d", fg="#e6b800")
+            minPriceLabel.grid(column=0, row=rows_used + 3, padx=10)
+
+            avgPriceLabel = Label(self.frame, text="Avg: " + str(self.price[1]), bg="#0d0d0d", fg="#e6b800")
+            avgPriceLabel.grid(column=1, row=rows_used + 3, padx=10)
+
+            maxPriceLabel = Label(self.frame, text="High: " + str(self.price[2]), bg="#0d0d0d", fg="#e6b800")
+            maxPriceLabel.grid(column=2, row=rows_used + 3, padx=10)
+
+"""
+gui = PriceInfo()
+gui.show(0,10)
+
+gui2 = GuiComponent()
+gui2.show(0,100)
+time.sleep(5)
+gui.close()
+gui.close()
+time.sleep(3)
+gui2.close()
+while True:
+    time.sleep(1)
+"""
